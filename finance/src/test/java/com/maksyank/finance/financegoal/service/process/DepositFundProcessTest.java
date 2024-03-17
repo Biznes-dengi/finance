a57114595d6c30f4828e322f5fe8c0feac26168e
Bronx666 <Hristyuk@outlook.com>
2024-03-18 02:49:00 +0300

    add test of FundProcess "input normal data"

--- finance/src/test/java/com/maksyank/finance/financegoal/service/process/DepositFundProcessTest.java
+++ finance/src/test/java/com/maksyank/finance/financegoal/service/process/DepositFundProcessTest.java
@@ -7,6 +7,7 @@ import com.maksyank.finance.financegoal.domain.enums.FinanceGoalState;
 import com.maksyank.finance.financegoal.domain.enums.TransactionType;
 import com.maksyank.finance.financegoal.exception.NotFoundException;
 import com.maksyank.finance.financegoal.service.repoimpl.FinanceGoalRepoImpl;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -17,19 +18,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 class DepositFundProcessTest {
 
     @InjectMocks
     private FundProcess fundProcess;
+
     @Mock
-    FinanceGoalRepoImpl financeGoalRepo;
+    private FinanceGoalRepoImpl financeGoalRepo;
 
     //    tests data
     final private FinanceGoal normalDataFinanceGoal =
@@ -37,16 +43,12 @@ class DepositFundProcessTest {
                     BigDecimal.ZERO, BigDecimal.ZERO, null, null, LocalDateTime.now(), null);
     ;
 
-
     //    creating deposits
     LocalDateTime fundingDate1 = LocalDateTime.of(2024, 3, 10, 9, 0);
-    Deposit deposit1 = new Deposit(TransactionType.FUND, "Initial deposit", fundingDate1, BigDecimal.valueOf(100),normalDataFinanceGoal);
+    Deposit deposit1 = new Deposit(TransactionType.FUND, "Initial deposit", fundingDate1, BigDecimal.valueOf(5), normalDataFinanceGoal);
 
     LocalDateTime fundingDate2 = LocalDateTime.of(2024, 3, 15, 12, 30);
-    Deposit deposit2 = new Deposit(TransactionType.FUND, "Additional deposit", fundingDate2, BigDecimal.valueOf(200),normalDataFinanceGoal);
-
-    // Создание списка deposits и добавление в него созданных объектов
-    List<Deposit> deposits = new ArrayList<>();
+    Deposit deposit2 = new Deposit(TransactionType.FUND, "Additional deposit", fundingDate2, BigDecimal.valueOf(15), normalDataFinanceGoal);
 
 
     @Test
@@ -59,26 +61,20 @@ class DepositFundProcessTest {
         int month = 1;
         int userId = 1;
 
+        Collection<Deposit> deposits = new ArrayList<>();
+        deposits.add(deposit1);
+        deposits.add(deposit2);
+        normalDataFinanceGoal.setDeposits(deposits);
+
 //        mock
-        Mockito.when(financeGoalRepo.findByIdAndUserId(1, 1)).thenReturn(normalDataFinanceGoal);
-
-//      I didn't find how to add deposit to FG
-//      and I don't understand how to get access to private method of FundProcess
-//
-
-        Mockito.when(fundProcess.findFundDepositsByMonth(normalDataFinanceGoal,
-                        LocalDateTime.of(year, month, 1, 0, 0, 0),
-                        LocalDateTime.of(year, month, YearMonth.of(year, month).lengthOfMonth(), 23, 59, 59)))
-                .thenReturn(deposits);
+        when(financeGoalRepo.findByIdAndUserId(1, 1)).thenReturn(normalDataFinanceGoal);
 
         //when
         BigDecimal result = fundProcess.processGetFundAmountByMonth(financeGoalId, year, month, userId);
 
-        BigDecimal expected = BigDecimal.ZERO; // Например, ожидается сумма 0, если список депозитов пустой
+        BigDecimal expected = BigDecimal.valueOf(20);
 
         //then
         assertEquals(expected, result, "The fund amount for the given month should match the expected value.");
-
-
     }
 }
